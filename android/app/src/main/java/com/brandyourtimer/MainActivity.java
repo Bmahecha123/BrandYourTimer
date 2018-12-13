69cd6c84b214fb7ee10f96614e0dba6c260b0424
Brian Mahecha <mahebd@mail.broward.edu>
2018-12-12 20:04:01 -0500

    Implemented full screen for Android

--- android/app/src/main/java/com/brandyourtimer/MainActivity.java
+++ android/app/src/main/java/com/brandyourtimer/MainActivity.java
@@ -1,6 +1,8 @@
 package com.brandyourtimer;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends ReactActivity {
 
@@ -30,7 +32,8 @@ public class MainActivity extends ReactActivity {
     private void hideNavigationBar() {
         getWindow().getDecorView().setSystemUiVisibility(
             View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
     }
 }
